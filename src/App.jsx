56195df523d51a764238d2cc4165413d98aef001
Rafael Caixeta <caixetadev@gmail.com>
2021-12-09 11:26:02 -0300

    consumindo API

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [moveList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="page">
+      <section className="lists">
+        {moveList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.item} />
+        ))}
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,55 @@
+const API_KEY = '53d3ee673d093d4311541837e6b4c437';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json()
+  return json;
+}
+
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        slug: 'originais',
+        title: 'Originais do Netflix',
+        item: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'trending',
+        title: 'Recomendados para Você',
+        item: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'toprated',
+        title: 'Em Alta',
+        item: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'action',
+        title: 'Ação',
+        item: await basicFetch(`/discover/movie?with_geners=28&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'comedy',
+        title: 'Comédia',
+        item: await basicFetch(`/discover/movie?with_geners=35&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        item: await basicFetch(`/discover/movie?with_geners=27&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        item: await basicFetch(`/discover/movie?with_geners=10749&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentários',
+        item: await basicFetch(`/discover/movie?with_geners=99&language=pt-BR&api_key=${API_KEY}`)
+      },
+    ]
+  }
+}
